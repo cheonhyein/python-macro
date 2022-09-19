5ad956f6066173d931c277eda783c43dbd06d831
cheon hyein <a_ae_o@naver.com>
2022-09-19 18:08:46 +0900

    add try catch

--- aming.py
+++ aming.py
@@ -3,17 +3,33 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from datetime import datetime
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+def clickCount() :
+    while True:
+        try:
+            selectboxoption = driver.find_element(By.XPATH, '//*[@id="prod_options"]/div/div/div[2]').click()
+            print("click시도")
+        except:
+            print("error")
+        else:
+            print("에러없음!")
+            break
+
+
+
+
+
+options = webdriver.ChromeOptions()
+# // 페이지 로딩에서 이미지 제외
+options.add_argument("--blink-settings=imagesEnabled=false")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.set_window_size(1400, 1000)
-driver.get('https://www.aimingmarket.com/?idx=335')
+driver.get('https://www.aimingmarket.com/?idx=337')
 # driver.switch_to.frame(driver.find_element(By.XPATH, "//div[@class='leftLoginBox']/iframe[@title='login']"))
 userId = driver.find_element(By.NAME, 'uid')
 userId.send_keys('GPDLS2574')
@@ -47,11 +63,21 @@ driver.implicitly_wait(10)
 #     else:
 #         if soldOut.text == '이 상품은 현재 판매기간이 아닙니다.':
 #             driver.refresh()
-#             time.sleep(0.1)
+#             time.sleep(0.05)
 #         else:
 #             break
 
-# time.sleep(0.1)
+# TODO test 중
+clickCount()
+
+
+
+
+
+
+
+print("try-except 탈출")
+
 # selectbox = driver.find_element(By.XPATH, '//*[@id="prod_options"]/div/div/div[2]').click()
 # time.sleep(0.1)
 # selectboxoption = driver.find_element(By.XPATH, '//*[@id="prod_options"]/div/div/div[2]/div/div[4]/a').click()
@@ -65,8 +91,7 @@ driver.implicitly_wait(10)
 # select2boxoption = driver.find_element(By.XPATH, '//*[@id="prod_options"]/div/div/div[2]/div/div[5]/a').click()
 
 
-sample = driver.find_element(By.XPATH, '//*[@id="prod_options"]/div/div/div[2]/div/div[5]/a')
-driver.execute_script("arguments[0].click();", sample)
+sample = driver.find_element(By.XPATH, '//*[@id="prod_options"]/div/div/div[2]')
 
 time.sleep(0.1)
 # select2boxoption = driver.find_element(By.XPATH, '//*[@id="prod_goods_form"]/div[8]/a[1]').click()
